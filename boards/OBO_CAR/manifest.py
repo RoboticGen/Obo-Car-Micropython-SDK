ffb196092f7816f0dd7a6990fdeabee975341f3f
Yasantha Niroshan <niroshanyi@gmail.com>
2025-01-30 00:35:41 +0530

    Added Obo-Car SDK - Added Obo-Car SDK as a submodule - Compiled Obo-Car SDK as bytecode - Removed xxd.py library

--- boards/OBO_CAR/manifest.py
+++ boards/OBO_CAR/manifest.py
@@ -2,4 +2,4 @@
 include("$(PORT_DIR)/boards/manifest.py")
 
 # include our own extra...
-module("xxd.py", base_path="$(BOARD_DIR)/../../src/utils")
+module("obocar.py", base_path="$(BOARD_DIR)/../../src/obo-car") 
