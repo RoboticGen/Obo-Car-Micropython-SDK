20e233b9e9a969dbd8a4f49271aa2cd94a581353
Yasantha Niroshan <niroshanyi@gmail.com>
2025-01-30 00:03:24 +0530

    Initialized Obo Micropython Firmware Builds - Updated main readme since missing some essential libraries - Removed additional custom boards - Removed c libraries - Added .gitignore to remove build dependencies - Added missing linker file for ESP32 - Updated micropython submodule

--- boards/OBO_CAR/mpconfigboard.cmake
+++ boards/OBO_CAR/mpconfigboard.cmake
@@ -7,11 +7,5 @@ set(SDKCONFIG_DEFAULTS
     ${MICROPY_PORT_DIR}/boards/sdkconfig.ble
 )
 
-# Set the user C modules to include in the build.
-set(USER_C_MODULES
-    ${PROJECT_DIR}/src/cmodules/cexample/micropython.cmake
-    ${PROJECT_DIR}/src/cmodules/cexample2/micropython.cmake
-)
-
 # Set the manifest file for frozen Python code.
 set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
