a3cfb286deea14ee7a6abda2442c2147182bffa0
Yasantha Niroshan <niroshanyi@gmail.com>
2025-02-03 00:18:17 +0530

    Added Major Updated - Workflow has been created to automate builing process - Logos has been restructured to compile into bytecode - Updated obo-car submodule branch to recieve updates from orgin

--- boards/OBO_CAR/manifest.py
+++ boards/OBO_CAR/manifest.py
@@ -2,4 +2,5 @@
 include("$(PORT_DIR)/boards/manifest.py")
 
 # include our own extra...
+module("logos.py", base_path="$(BOARD_DIR)/../../src/obo-car")
 module("obocar.py", base_path="$(BOARD_DIR)/../../src/obo-car") 
